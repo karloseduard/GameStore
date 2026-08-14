36f8fdefb56e7d0e452e3694a349241c79928d58
Karlos Eduardo <92446047+karloseduard@users.noreply.github.com>
2026-08-14 16:31:27 -0300

    Add endpoint para o update games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 const string GetGameEndPointName = "GetGame";
 var app = builder.Build();
 
-List<GameDto> gmaes = [
+List<GameDto> games = [
     new (
         1,
         "Hollow Knight",
@@ -25,26 +25,43 @@ List<GameDto> gmaes = [
         new DateOnly(2017,9,29))
 ];
 // GET /games
-app.MapGet("/games", () => gmaes);
+app.MapGet("/games", () => games);
 // GET /games/id
-app.MapGet("/games/{id}", (int id) => gmaes.Find(game => game.Id == id))
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
 .WithName(GetGameEndPointName);
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
 {
      GameDto game = new(
-        gmaes.Count + 1,
+     games.Count + 1,
         newGame.Name,
         newGame.Genre,
         newGame.Price,
         newGame.ReleaseDate
     );
 
-    gmaes.Add(game);
+ games.Add(game);
 
     return Results.CreatedAtRoute( GetGameEndPointName , new {id = game.Id}, game);
 }
-   
+
 );
+
+//PUT /game/{id}
+
+app.MapPut("/games/{id}", (int id, UpdateGameDto updateGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updateGame.Name,
+        updateGame.Genre,
+        updateGame.Price,
+        updateGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
 
 app.Run();
