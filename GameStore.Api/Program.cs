a207496fd3ccfd34b94fe654ca580e245451a7e3
Karlos Eduardo <92446047+karloseduard@users.noreply.github.com>
2026-08-19 16:42:57 -0300

    Add Entity Framework Core, Implementing the data model and Entity Framework Core integration

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,14 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connString = "Data Source=GameStore.db";
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
