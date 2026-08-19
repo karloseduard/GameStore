911eb7f181765fbe3b25fd3cb1683068effb7f49
Karlos Eduardo <92446047+karloseduard@users.noreply.github.com>
2026-08-19 12:14:07 -0300

    Adding input validation

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a25338de43fa3fce9c0af6903ae1ffd63dbeec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cad77348fbeca49fcb279d2d34f0f76bf484479")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
