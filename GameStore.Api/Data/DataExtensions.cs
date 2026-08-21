a9acb8ccdc16c38d7af226a12fb78965ebb62014
Karlos Eduardo <92446047+karloseduard@users.noreply.github.com>
2026-08-21 10:48:54 -0300

    It was inserted into the database's genres table.

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -9,6 +10,30 @@ public static class DataExtensions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider
             .GetRequiredService<GameStoreContext>();
-            dbContext.Database.Migrate();
+        dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data Source=GameStore.db";
+
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Plataformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" }
+
+                    );
+                    context.SaveChanges();
+                }
+            })
+            );
     }
 }
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a207496fd3ccfd34b94fe654ca580e245451a7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a15825d2cdb674f0829dda258bd3d249a49678")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
