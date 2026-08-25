85f26d6e2f421690e67d15f334269512009318de
Karlos Eduardo <92446047+karloseduard@users.noreply.github.com>
2026-08-25 17:32:46 -0300

    Add Dependency Injection and database support to API endpoints

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,4 +1,6 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
+using GameStore.Api.Models;
 
 namespace GameStore.Api.Endpoints;
 
@@ -42,19 +44,28 @@ public static class GamesEndpoints
         .WithName(GetGameEndPointName);
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame) =>
+        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
-            GameDto game = new(
-            games.Count + 1,
-               newGame.Name,
-               newGame.Genre,
-               newGame.Price,
-               newGame.ReleaseDate
-           );
+            Game game = new()
+            {
+                Name = newGame.Name,
+                GenreId = newGame.GenreId,
+                Price = newGame.Price,
+                ReleaseDate = newGame.ReleaseDate
+            };
 
-            games.Add(game);
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
 
-            return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+            GameDetailsDto gameDto = new(
+                game.Id,
+                game.Name,
+                game.GenreId,
+                game.Price,
+                game.ReleaseDate
+            );
+
+            return Results.CreatedAtRoute(GetGameEndPointName, new { id = gameDto.Id }, gameDto);
         }
 
         );
